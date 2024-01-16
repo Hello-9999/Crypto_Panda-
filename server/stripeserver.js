c732a354ad57cddc1c7a1f4a143f921ded6b6345
Gautam Magar <puskarmagar687@gmail.com>
2024-01-16 19:55:55 +0545

    Adding env and port

--- server/stripeserver.js
+++ server/stripeserver.js
@@ -166,6 +166,7 @@ app.post("/api/create-checkout-session", async (req, res) => {
 //   }
 // });
 
-app.listen(7000, () => {
+const  PORT = process.env.PORT || 7000
+app.listen(PORT, () => {
   console.log("Server is running on port 7000");
 });
